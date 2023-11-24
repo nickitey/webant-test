e01cc2ae163a13cb996cbb14ea3979f800752bc1
Nikita Savin <klaster26@mail.ru>
2023-11-24 23:16:02 +0300

    feat: add scripts to 'locations'
    Carefully! Filters are not working well.

--- locations/index.js
+++ locations/index.js
@@ -1,3 +1,9 @@
+function createElement(html) {
+    const div = document.createElement('div');
+    div.innerHTML = html;
+    return div.firstElementChild;
+  };
+
 function switcher() {
   const navMenu = document.querySelector('.main-navigation');
   const navList = document.querySelector('.nav-list');
@@ -23,13 +29,68 @@ function switcher() {
   })
 }
 
-function contentMaker() {
-  window.addEventListener('resize', () => {
-    const examples = document.querySelector('.examples');
-    if (document.body.clientWidth <= 800) {
-      examples.innerHTML = 'hello,world!';
-    } else {
-      examples.innerHTML = 'it\'s so fuckin\' big!';
-    }
-  });
+class ExampleCard {
+  elem;
+  #example = {};
+  constructor(example) {
+    this.elem = null;
+    this.#example = example || this.#example;
+    this.#render();
+  }
+  #template () {
+    return (
+    `<div class="grid-element">
+      <a href="" class="card-link">${this.#example.name} ${this.#example.dimension}</a>
+      <p class="card-text">${this.#example.type}</p>
+     </div>`
+    );
+  }
+  #render () {
+    this.elem = createElement(this.#template());
+  }
 }
+
+class ExampleGrid {
+  elem;
+
+  constructor(examples) {
+    this.examples = examples;
+    this.elem = null;
+    this.filters = {};
+    this.#render();
+  }
+
+  #render() {
+    this.elem = createElement(
+      `<section class="examples"></section>`
+    );
+    this.#elementContent();
+  }
+
+  #elementContent() {
+    let inner = this.elem.closest(`.examples`);
+    inner.innerHTML = '';
+    for (let item of this.examples) {
+      if (this.filters.name && item.name != this.filters.name) {
+          continue;
+        }
+      if (this.filters.dimension && item.dimension != this.filters.dimension) {
+          continue;
+        }
+      if (this.filters.type && item.type != this.filters.type) {
+          continue;
+        }
+      if (this.filters.amount && this.elem.querySelectorAll('.grid-element').length >= this.filters.amount) {
+        continue;
+      }
+      let exampleItem = new ExampleCard(item);
+      inner.append(exampleItem.elem);
+    }
+  }
+
+  updateFilter(filters) {
+    Object.assign(this.filters, filters);
+    this.#elementContent();
+  }
+}
+
